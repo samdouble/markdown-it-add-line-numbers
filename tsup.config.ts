49988b6f93cb66af6e635522c4d9bff727c1faa6
samdouble <samleafs@protonmail.com>
2025-04-20 20:44:35 -0400

    fix: export files as cjs format during build

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,12 @@ export default defineConfig({
   clean: true,
   dts: true,
   entry: ['src/index.ts'],
+  esbuildOptions(options) {
+    options.format = 'cjs',
+    options.target = 'node14'
+  },
   format: ['cjs', 'esm'],
+  outDir: 'dist',
   splitting: false,
   sourcemap: true,
 });
